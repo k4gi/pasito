ce804369a4ce2840c7a38e3c84e4ac07ca77e575
kagi <k4gi@pm.me>
2021-01-04 10:54:04 +1100

    completed part 2 that works as it should

--- AnimationPlayer_Manager.gd
+++ AnimationPlayer_Manager.gd
@@ -3,60 +3,60 @@ extends AnimationPlayer
 # Structure -> Animation name :[Connecting Animation states]
 var states = {
 	"Idle_unarmed":["Knife_equip", "Pistol_equip", "Rifle_equip", "Idle_unarmed"],
- 
+
 	"Pistol_equip":["Pistol_idle"],
 	"Pistol_fire":["Pistol_idle"],
 	"Pistol_idle":["Pistol_fire", "Pistol_reload", "Pistol_unequip", "Pistol_idle"],
 	"Pistol_reload":["Pistol_idle"],
 	"Pistol_unequip":["Idle_unarmed"],
- 
+
 	"Rifle_equip":["Rifle_idle"],
 	"Rifle_fire":["Rifle_idle"],
 	"Rifle_idle":["Rifle_fire", "Rifle_reload", "Rifle_unequip", "Rifle_idle"],
 	"Rifle_reload":["Rifle_idle"],
 	"Rifle_unequip":["Idle_unarmed"],
- 
+
 	"Knife_equip":["Knife_idle"],
 	"Knife_fire":["Knife_idle"],
 	"Knife_idle":["Knife_fire", "Knife_unequip", "Knife_idle"],
 	"Knife_unequip":["Idle_unarmed"],
 }
- 
+
 var animation_speeds = {
 	"Idle_unarmed":1,
- 
+
 	"Pistol_equip":1.4,
 	"Pistol_fire":1.8,
 	"Pistol_idle":1,
 	"Pistol_reload":1,
 	"Pistol_unequip":1.4,
- 
+
 	"Rifle_equip":2,
 	"Rifle_fire":6,
 	"Rifle_idle":1,
 	"Rifle_reload":1.45,
 	"Rifle_unequip":2,
- 
+
 	"Knife_equip":1,
 	"Knife_fire":1.35,
 	"Knife_idle":1,
 	"Knife_unequip":1,
 }
- 
+
 var current_state = null
 var callback_function = null
- 
+
 func _ready():
 	set_animation("Idle_unarmed")
 	connect("animation_finished", self, "animation_ended")
- 
+
 func set_animation(animation_name):
 	if animation_name == current_state:
 		print ("AnimationPlayer_Manager.gd -- WARNING: animation is already ", animation_name)
 		return true
- 
- 
-	if has_animation(animation_name):
+
+
+	if has_animation(animation_name) == true:
 		if current_state != null:
 			var possible_animations = states[current_state]
 			if animation_name in possible_animations:
@@ -71,10 +71,10 @@ func set_animation(animation_name):
 			play(animation_name, -1, animation_speeds[animation_name])
 			return true
 	return false
- 
- 
+
+
 func animation_ended(anim_name):
- 
+
 	# UNARMED transitions
 	if current_state == "Idle_unarmed":
 		pass
@@ -109,7 +109,7 @@ func animation_ended(anim_name):
 		set_animation("Idle_unarmed")
 	elif current_state == "Rifle_reload":
 		set_animation("Rifle_idle")
- 
+
 func animation_callback():
 	if callback_function == null:
 		print ("AnimationPlayer_Manager.gd -- WARNING: No callback function for the animation to call!")
